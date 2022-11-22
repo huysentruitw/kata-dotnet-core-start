98914fd8e9f71895f5b68b274b19296198c0885f
Jan Slabbinck <jan.slabbinck@gantner.com>
2022-11-22 10:46:31 +0100

    FirstElementShouldBeOne

--- kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
+++ kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
@@ -1,10 +1,19 @@
+
+using FluentAssertions;
+
 namespace kata_dotnet_core_start.Lib.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void FirstElementShouldBeOne()
     {
-        Assert.Equal(true, false);
+        // Arrange
+
+        // Act
+        var result = FizzBuzz.Generate();
+
+        // Assert
+        result.First().Should().Be("1");
     }
 }
---
+++ kata-dotnet-core-start.Lib/FizzBuzz.cs
@@ -0,0 +1,8 @@
+﻿namespace kata_dotnet_core_start.Lib;
+public static class FizzBuzz
+{
+    public static IEnumerable<string> Generate()
+    {
+        yield return "1";
+    }
+}
